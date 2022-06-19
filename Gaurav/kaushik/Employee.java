be0eb397d6bbcb763b985adf1e5b05ab3c856a8b
Connectgauravkaushik <101702222+Connectgauravkaushik@users.noreply.github.com>
2022-06-19 21:06:06 +0530

    Add files via upload

---
+++ Gaurav/kaushik/Employee.java
@@ -0,0 +1,41 @@
+package com.Gaurav.kaushik;
+
+public class Employee {
+private String FirstName;
+private String LastName;
+
+public Employee(String firstName, String lastName) {
+	FirstName = firstName;
+	LastName = lastName;
+}
+
+public String getFirstName() {
+	return FirstName;
+}
+public void setFirstName(String firstName) {
+	FirstName = firstName;
+}
+public String getLastName() {
+	return LastName;
+}
+public void setLastName(String lastName) {
+	LastName = lastName;
+}
+public void Technical() {
+}
+
+public void Admin() {
+	
+	
+}
+
+public void HumanResource() {
+	
+	
+}
+
+public void Legal() {
+	  
+}
+
+}
